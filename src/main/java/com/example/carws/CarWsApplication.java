6139c388a77f1e7611f54edfea5cef44ea5c5a91
Tendrynyavo <143934345+tendrynyavo@users.noreply.github.com>
2024-01-06 11:00:53 +0300

    first commit

---
+++ src/main/java/com/example/carws/CarWsApplication.java
@@ -0,0 +1,13 @@
+package com.example.carws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/carws/CarWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
